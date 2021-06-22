6e7939d1dd304f89d6d6f31d19464c16548f4d10
karolcieply <72377467+karolcieply@users.noreply.github.com>
2021-06-22 13:30:06 +0200

    Add files via upload

---
+++ Project1/Definitions.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#define windowWidth 540
+#define windowHeight 960
+#define frameLimit 60
+#define FONT_FILE "./resources/font.ttf"
+#define BACKGROUND_FILE "./resources/background.png"
+#define TIMBERMAN_IDLE_SPRITE "./resources/firstSkinIdle.png"
+#define TIMBERMAN_CHOP_SPRITE "./resources/firstSkinChop.png"
+#define TREE_LEFT_TEXTURE "./resources/treeleft.png"
+#define TREE_RIGHT_TEXTURE "./resources/treeright.png"
+#define TREE_NEUTRAL_TEXTURE "./resources/tree.png"
+#define START_TEXTURE "./resources/start.png"
+#define EXIT_TEXTURE "./resources/exit.png"
+#define TIMER_BORDER_TEXTURE "./resources/TimerBorder.png"
+#define GAME_OVER_TEXTURE "./resources/GameOver.png"
+#define HIGH_SCORES_FILE "./resources/highScores.txt"
+#define HIGH_SCORES_BACKGROUND "./resources/highscorebackground.png"
+#define HIGH_SCORES_BUTTON "./resources/highscore.png"
+#define PLAY_AGAIN_BUTTON "./resources/playagain.png"
+#define MAIN_MENU_BUTTON "./resources/menu.png"
+#define RULES_BUTTON "./resources/rules.png"
+#define SMALL_EXIT_BUTTON "./resources/smallExit.png"
+#define RULES_BACKGROUND "./resources/rulesBackground.png"
+#define SHOP_BUTTON "./resources/shop.png"
+#define TREE_CHOP_SOUND "./resources/treeChopSound.wav"
+#define BACKGROUND_MUSIC "./resources/backgroundMusic.wav"
+#define ARROW_BUTTON "./resources/rightArrow.png"
+#define BUTTON_CLICK_SOUND "./resources/buttonClickSound.wav"
+#define NEW_BEST_SCORE_SOUND "./resources/newBestScoreSound.wav"
+#define GAME_OVER_SOUND "./resources/gameOverSound.wav"
+#define PAUSE_BACKGROUND "./resources/pauseBackground.png"
+#define CONTINUE_BUTTON "./resources/continue.png"
